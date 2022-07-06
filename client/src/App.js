c47bc15ef320157c9635de12faea88ddc6add6e0
emilioalvarez05 <emilioadrianalvarez05@gmail.com>
2022-07-06 18:48:59 -0300

    configClient

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,6 @@ function App() {
       <h1>Henry Countries</h1>
     </div>
   );
-}
+} 
 
 export default App;
---
+++ client/src/store/reducer/index.js
@@ -0,0 +1,20 @@
+
+
+const initialState = {
+    countries: [],
+    allCountries: [],
+    filter: {},
+    countriesDetail: {},
+    touristActivity: []
+}
+
+export default function reducer(state = initialState, {type, payload}){
+    switch (type) {
+        
+
+        default:
+            return state;
+    }
+
+
+}
